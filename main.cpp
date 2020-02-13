ed7b08e3caea5b640fb5df39d4079d3182888937
כjackkohav <jackkohav@gmail.com>
2020-02-13 17:55:51 +0200

    Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Transformation matrix of lame Ido!" << std::endl;
+    std::cout << "Transformation matrix of lame Ido." << std::endl;
     return 0;
 }
