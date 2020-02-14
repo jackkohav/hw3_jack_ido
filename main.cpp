57a205b04fbfe8ba9f3a0f2edb48a4810f400c8f
idogoorevitch <idogoorevitch@gmail.com>
2020-02-14 15:23:32 +0200

    changes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Transformation matrix of very cool though stupid Ido?" << std::endl;
+    std::cout << " ^_^ " << std::endl;
     std::cout << "Transformation matrix of very cool though stupid Ido!!!!" << std::endl;
     return 0;
 }
