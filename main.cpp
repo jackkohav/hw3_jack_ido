2c0f69963897b58ab63d10e84edff2a0de6edfab
כjackkohav <jackkohav@gmail.com>
2020-02-14 15:29:01 +0200

    Wrote randomFunc

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+int randomFunc(int a){
+    return a - 3*a*a + 9;
+}
+
 int main() {
     std::cout << " ^_^ " << std::endl;
     std::cout << "Transformation matrix of very cool though stupid Ido!!!!" << std::endl;
