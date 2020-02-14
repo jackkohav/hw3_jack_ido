1b44d3d5e59fbf0dc573535c5e7da6063391a415
כjackkohav <jackkohav@gmail.com>
2020-02-14 15:04:10 +0200

    Attempt

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-using namespace std;
 
 int main() {
-    cout << "High-five against Yahav" << endl;
+    std::cout << "Transformation matrix of very cool though stupid Ido!" << std::endl;
     return 0;
 }
