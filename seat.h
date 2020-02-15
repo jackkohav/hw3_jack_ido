4e64802be53cf7ece519147d36ee99dd3afc19ca
כjackkohav <jackkohav@gmail.com>
2020-02-15 23:34:42 +0200

    continued wet part 3

--- seat.h
+++ seat.h
@@ -2,7 +2,7 @@
 #include <string>
 using std::string;
 using std::exception;
-
+#define NO_PRICE -1
 
 // ---------------------------------------------
 class NoPrice
@@ -12,19 +12,29 @@ class NoPrice
 // ---------------------------------------------
 class Seat
 {
-    int row_number;
+    int line_number;
     int chair_number;
     int base_price;
 public:
-    Seat(int row, int chair, int base):
-        row_number(row), chair_number(chair), base_price(base){}
+    Seat(int line, int chair, int base):
+        line_number(line), chair_number(chair), base_price(base){}
     virtual int price() const = 0;
-
+    virtual string location() const = 0;
 };
 
 // ---------------------------------------------
 class GreenRoomSeat
 {
+public:
+    GreenRoomSeat(int line, int chair):
+        Seat(line, chair, NO_PRICE){}
+    int price() const override {
+        throw NoPrice();
+    }
+    string location() const override {
+        return "Green Room-> line: " + std::to_string(line_number) + ", chair: " +
+           std::to_string(chair_number);
+    }
 };
 
 // ---------------------------------------------
