9fbc39c1c568c59d8281a61a44dc0aa144c8044b
כjackkohav <jackkohav@gmail.com>
2020-02-15 12:12:40 +0200

    Started Question 1 b

--- DryQ1.cpp
+++ DryQ1.cpp
@@ -1,4 +1,27 @@
 //
 // Created by jack on 15/02/2020.
 //
+#include <iostream>
+#include <vector>
+using namespace std;
 
+template<class T, class Predicate>
+int numberOfPairs(const T& container, Predicate pred){
+
+}
+
+int fact(unsigned const int n){
+    if(n == 0) return 1;
+    return fact(n-1);
+}
+
+int choose(unsigned const int n, unsigned const int k){
+    if(k > n) return 0;
+    return fact(n)/(fact(k)*fact(n-k));
+}
+
+bool monotone();
+
+bool isSorted(vector<int> v){
+    return true;
+}
