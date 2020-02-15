a00dc0eb2b56d210a7e8c472e39de3177c2258e7
כjackkohav <jackkohav@gmail.com>
2020-02-15 09:47:21 +0200

    Wrote the code of question 2 of the dry part in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
-
-int randomFunc(int a){
-    return a - 3*a*a + 9;
-}
+class Base
+{
+    void method(){
+        std::cout << "from Base" << std::endl;
+    }
+public:
+    virtual ~Base(){
+        method();
+    }
+    void baseMethod(){
+        method();
+    }
+};
+class A: public Base
+{
+    void method(){
+        std::cout << "from A" << std::endl;
+    }
+public:
+    ~A() override {
+        method();
+    }
+};
 
 int main() {
-    std::cout << " ^_^ " << std::endl;
-    std::cout << "Everything is awesome" << std::endl;
+    Base* base = new A();
+    base->baseMethod();
+    delete base;
     return 0;
-}
-
-void ido(int a){
-    if(a>0){
-        std::cout << "  (<:|>  " << std::endl;
-    }
-    std::cout << " ido " << std::endl;
 }
